49e849e840a71b0406360c9cd30f6f3683d4da8e
Martin Donadieu <martindonadieu@gmail.com>
2023-05-31 04:12:42 +0100

    feat: add pip

--- ios/Plugin/CapacitorIvsPlayerPlugin.swift
+++ ios/Plugin/CapacitorIvsPlayerPlugin.swift
@@ -6,17 +6,14 @@ import AmazonIVSPlayer
 class MyIVSPlayerDelegate: NSObject, IVSPlayer.Delegate {
 
     func player(_ player: IVSPlayer, didChangeState state: IVSPlayer.State) {
-//        updateForState(state)
         print("state change")
     }
 
     func player(_ player: IVSPlayer, didFailWithError error: Error) {
-//        presentError(error, componentName: "Player")
         print("error change")
     }
 
     func player(_ player: IVSPlayer, didChangeDuration duration: CMTime) {
-//        updateForDuration(duration: duration)
         print("duration change")
     }
 
@@ -47,14 +44,27 @@ public class CapacitorIvsPlayerPlugin: CAPPlugin {
     let playerDelegate = MyIVSPlayerDelegate()
     let playerView = IVSPlayerView()
     
+    private var _pipController: Any? = nil
+
+    @available(iOS 15, *)
+    private var pipController: AVPictureInPictureController? {
+       get {
+           return _pipController as! AVPictureInPictureController?
+       }
+       set {
+           _pipController = newValue
+       }
+   }
+    
     @objc func echo(_ call: CAPPluginCall) {
         let value = call.getString("value") ?? ""
         print("hello")
         player.delegate = playerDelegate
-//        playerView.player = player
         player.load(URL(string:"https://d6hwdeiig07o4.cloudfront.net/ivs/956482054022/cTo5UpKS07do/2020-07-13T22-54-42.188Z/OgRXMLtq8M11/media/hls/master.m3u8"))
+
         DispatchQueue.main.async {
             self.playerView.player = self.player
+            self.preparePictureInPicture()
             self.player.play()
             guard let viewController = self.bridge?.viewController else {
                 call.reject("Unable to access the view controller")
@@ -64,18 +74,31 @@ public class CapacitorIvsPlayerPlugin: CAPPlugin {
             self.playerView.frame = viewController.view.bounds
             viewController.view.addSubview(self.playerView)
         }
-//        let playerView = player.view
-//        playerView.frame = viewController.view.bounds
-//        viewController.view.addSubview(playerView)
-//        DispatchQueue.main.async {
-//            self.webView?.backgroundColor = UIColor.clear
-//            self.webView?.isOpaque = false
-//            self.webView?.scrollView.backgroundColor = UIColor.clear
-//            self.webView?.scrollView.isOpaque = false
-//            self.webView?.superview?.bringSubviewToFront(self.webView!)
-//        }
         call.resolve([
             "value": implementation.echo(value)
         ])
+    }
+    
+    private func preparePictureInPicture() {
+
+        guard #available(iOS 15, *), AVPictureInPictureController.isPictureInPictureSupported() else {
+            return
+        }
+
+
+        if let existingController = self.pipController {
+            if existingController.ivsPlayerLayer == playerView.playerLayer {
+                return
+            }
+            self.pipController = nil
+        }
+
+        guard let pipController = AVPictureInPictureController(ivsPlayerLayer: playerView.playerLayer) else {
+            return
+        }
+
+        self.pipController = pipController
+        pipController.canStartPictureInPictureAutomaticallyFromInline = true
+
     }
 }
