fc9b84c04f38e8ddeac1180c530e28d86ecefcab
Martin Donadieu <martindonadieu@gmail.com>
2023-06-01 21:15:18 +0100

    WIP player

--- android/src/main/java/ee/forgr/ivsplayer/CapacitorIvsPlayer.kt
+++ android/src/main/java/ee/forgr/ivsplayer/CapacitorIvsPlayer.kt
@@ -1,10 +1,48 @@
 package ee.forgr.ivsplayer
 
+import android.app.PictureInPictureParams
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.amazonaws.ivs.player.PlayerView
 
-class CapacitorIvsPlayer {
-    fun echo(value: String): String {
-        Log.i("Echo", value)
-        return value
+// extends with AppCompatActivity
+class CapacitorIvsPlayer: AppCompatActivity() {
+    private lateinit var playerView : PlayerView
+    private lateinit var context: Context
+
+    fun init(context: Context) {
+        this.context = context
+        this.playerView = PlayerView(this)
+        playerView.player.load(Uri.parse("https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.DmumNckWFTqz.m3u8"))
+
+    }
+
+    fun togglePip(){
+        Log.i("PIP", "PIP")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+                && context.packageManager
+                        .hasSystemFeature(
+                                PackageManager.FEATURE_PICTURE_IN_PICTURE)) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val params = PictureInPictureParams.Builder()
+                this.enterPictureInPictureMode(params.build());
+            } else {
+                this.enterPictureInPictureMode();
+            }
+
+        }
+    }
+    fun play(){
+        playerView.player.play()
+    }
+    fun pause(){
+        playerView.player.pause()
+    }
+    fun release(){
+        playerView.player.release()
     }
 }
--- android/src/main/java/ee/forgr/ivsplayer/CapacitorIvsPlayerPlugin.kt
+++ android/src/main/java/ee/forgr/ivsplayer/CapacitorIvsPlayerPlugin.kt
@@ -1,12 +1,9 @@
 package ee.forgr.ivsplayer
 
-import android.app.Activity
-import android.app.PictureInPictureParams
-import android.content.pm.PackageManager
-import android.net.Uri
+import android.app.KeyguardManager
+import android.content.Context
+import android.content.Intent
 import android.os.Build
-import android.util.Log
-import com.amazonaws.ivs.player.*
 import com.getcapacitor.JSObject
 import com.getcapacitor.Plugin
 import com.getcapacitor.PluginCall
@@ -17,49 +14,35 @@ import com.getcapacitor.annotation.CapacitorPlugin
 @CapacitorPlugin(name = "CapacitorIvsPlayer")
 class CapacitorIvsPlayerPlugin : Plugin() {
     private val implementation = CapacitorIvsPlayer()
-    private lateinit var playerView : PlayerView
     @PluginMethod
     fun echo(call: PluginCall) {
-        val value = call.getString("value")
+//        implementation
+//        implementation.init(this.context)
         val ret = JSObject()
-        ret.put("value", implementation.echo(value!!))
         call.resolve(ret)
+        val intent = Intent(context, CapacitorIvsPlayer::class.java)
 
-        val playerView = PlayerView(this.activity)
-        playerView.player.load(Uri.parse("https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.DmumNckWFTqz.m3u8"))
-
+        startActivityForResult(call, intent, "verifyResult")
     }
 
     @PluginMethod
     fun togglePip(call: PluginCall){
-        Log.i("PIP", "PIP")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
-                && context.packageManager
-                        .hasSystemFeature(
-                                PackageManager.FEATURE_PICTURE_IN_PICTURE)) {
-            val activity: Activity? = this.activity
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val params = PictureInPictureParams.Builder()
-                activity?.enterPictureInPictureMode(params.build());
-            } else {
-                activity?.enterPictureInPictureMode();
-            }
-
-        }
+        implementation.togglePip()
+        call.resolve()
     }
 
     @PluginMethod
     fun start(call: PluginCall) {
-        playerView.player.play()
+        implementation.play()
     }
 
     @PluginMethod
     fun stop(call: PluginCall) {
-        playerView.player.pause()
+        implementation.pause()
     }
 
     @PluginMethod
     fun destroy(call: PluginCall) {
-        playerView.player.release()
+        implementation.release()
     }
 }
