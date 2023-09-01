b34ce730689133683008b265db525badf11477bb
Martin Donadieu <martindonadieu@gmail.com>
2023-09-01 16:58:40 +0200

    fix: cast issue

--- android/src/main/java/ee/forgr/ivsplayer/CastOptionsProvider.java
+++ android/src/main/java/ee/forgr/ivsplayer/CastOptionsProvider.java
@@ -1,6 +1,8 @@
 package ee.forgr.ivsplayer;
 
 import android.content.Context;
+
+import com.google.android.gms.cast.CastMediaControlIntent;
 import com.google.android.gms.cast.framework.CastOptions;
 import com.google.android.gms.cast.framework.OptionsProvider;
 import com.google.android.gms.cast.framework.SessionProvider;
@@ -11,6 +13,7 @@ public class CastOptionsProvider implements OptionsProvider {
   @Override
   public CastOptions getCastOptions(Context context) {
     CastOptions castOptions = new CastOptions.Builder()
+    .setReceiverApplicationId(CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID)
       //                .setReceiverApplicationId(context.getPackageName().replace(".", "_"))
       .build();
     return castOptions;
